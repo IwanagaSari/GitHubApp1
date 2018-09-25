5ad789b98657ddd42d4aa6cc98bd2e2a08d8c60a
岩永 彩里 <iwanaga.sari@mf-c0092-mm01.local>
2018-09-25 19:54:48 +0900

    Initial Commit

---
+++ GitHubApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubApp
+//
+//  Created by 岩永 彩里 on 2018/09/25.
+//  Copyright © 2018年 岩永 彩里. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
