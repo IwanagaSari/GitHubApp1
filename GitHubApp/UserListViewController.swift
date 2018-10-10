edcd0d8bae1910a245cefb8becbb58403e72fba8
岩永彩里 <catherinrin@gmail.com>
2018-10-10 16:45:38 +0900

    通信処理とビューに表示する処理を分離してみた。

--- GitHubApp/UserListViewController.swift
+++ GitHubApp/UserListViewController.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 class UserListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+
     @IBOutlet weak var userListTabelView: UITableView!
-    
-    struct User: Codable{
+
+    struct User: Codable {
         let login: String
         let avatar_url: String
     }
-    
-    var users: [User] = []{
+
+    var users: [User] = [] {
         didSet {
             userListTabelView.reloadData()
         }
@@ -27,50 +27,56 @@ class UserListViewController: UIViewController, UITableViewDelegate, UITableView
             //userListTabelView.reloadData()
         //}
     //}
-    
-    var selectedUserName :String?
-    var selectedUserFullName :String?
-    var accessToken :String = ""
-    
+
+    var selectedUserName: String?
+    var selectedUserFullName: String?
+    var accessToken: String = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         userListTabelView.delegate = self
         userListTabelView.dataSource = self
-        
-        var req = URLRequest(url: URL(string: "https://api.github.com/users")!)
-        req.addValue("token \(accessToken)", forHTTPHeaderField: "Authorization")
-        
-        let task: URLSessionTask = URLSession.shared.dataTask(with: req, completionHandler: {data, response, error in
-            do {
-                //print(String(data: data!, encoding: .utf8))
-                let jsons: [User] = try JSONDecoder().decode([User].self, from: data!)
-                //let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments) as! [Any]
-                
-                //let articles = json.map { (article) -> [String: Any] in
-                    //return article as! [String: Any]
-                //}
-                DispatchQueue.main.async() { () -> Void in
-                //self.users = articles
-                    self.users = jsons
-                }
-            }
-            catch {
-                print(error)
-            }
-        })
-        task.resume() //実行する
-        // Do any additional setup after loading the view.
+
+        // ビューに表示
+        fetchUsers { _, _ in
+        }
+
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         self.title = "User"
     }
-    
+
+    //通信処理(とデータ変換)
+    //class GitHubAPI {  これをつけるとaccessTokenが使えなかった。
+         func fetchUsers(completion: (([User]?, Error?) -> Void)) {
+            var req = URLRequest(url: URL(string: "https://api.github.com/users")!)
+            req.addValue("token \(accessToken)", forHTTPHeaderField: "Authorization")
+
+             //let task = URLSession.shared.dataTask(with: req) { data, _, error in
+             let task: URLSessionTask = URLSession.shared.dataTask(with: req, completionHandler: {data, _, error in
+                // ここにレスポンスの状態(エラーとか、正常とか)をみる処理があるとなお良いと思います
+                let decoder = JSONDecoder()
+             do {
+                let users: [User] = try decoder.decode([User].self, from: data!)
+
+                DispatchQueue.main.async { () -> Void in
+                    //self.users = articles
+                    self.users = users
+                }
+
+             } catch {
+                print(error)
+                }
+            })
+            task.resume()
+         }
+    //}
+
     //行数の指定
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return users.count
     }
      //セクションの数
@@ -80,49 +86,38 @@ class UserListViewController: UIViewController, UITableViewDelegate, UITableView
     //セルの内容
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "myCell")
-       
+
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "myCell")
-        
+
         let article = users[indexPath.row]
         let userName = article.login
         let userImage = article.avatar_url
         let userImageURL: URL = URL(string: "\(userImage)")!
         let imageData = try? Data(contentsOf: userImageURL)
-        
+
         cell.textLabel?.text = "\(userName)"
         cell.imageView?.image = UIImage(data: imageData!)
-    
+
     return cell
     }
-    
     //セルの高さ
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
-    
     //セル選択時
      func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+
         let article = users[indexPath.row]
         selectedUserName = article.login
-        
+
         performSegue(withIdentifier: "toUserRepositoryList", sender: IndexPath.self)
-        
     }
-    
     //次のページへ値の受け渡し
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        let userRepositoryListViewController = segue.destination as!UserRepositoryListViewController
-        userRepositoryListViewController.nameLabel = selectedUserName
-        userRepositoryListViewController.accessToken = accessToken
-        
-        
+        let userRepositoryListViewController = segue.destination as?UserRepositoryListViewController
+        userRepositoryListViewController?.nameLabel = selectedUserName
+        userRepositoryListViewController?.accessToken = accessToken
     }
-    
-   
-    
-
     /*
     // MARK: - Navigation
 
