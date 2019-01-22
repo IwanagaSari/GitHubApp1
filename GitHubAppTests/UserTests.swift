c768d8152110ffe7fb567b5734415b1fa98f5cca
岩永 彩里 <>
2019-01-22 21:12:06 +0900

    User一覧をJSONから取得する際、キーとバリューが一致しているか

---
+++ GitHubAppTests/UserTests.swift
@@ -0,0 +1,58 @@
+//
+//  UserTests.swift
+//  GitHubAppTests
+//
+//  Created by 岩永 彩里 on 2019/01/22.
+//  Copyright © 2019年 岩永 彩里. All rights reserved.
+//
+
+import XCTest
+@testable import GitHubApp
+
+class UserTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        
+        let testJson1: [String: Any] = [
+            "login": "octocat",
+            "id": 1,
+            "node_id": "MDQ6VXNlcjE=",
+            "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/octocat",
+            "html_url": "https://github.com/octocat",
+            "followers_url": "https://api.github.com/users/octocat/followers",
+            "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+            "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+            "organizations_url": "https://api.github.com/users/octocat/orgs",
+            "repos_url": "https://api.github.com/users/octocat/repos",
+            "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/octocat/received_events",
+            "type": "User",
+            "site_admin": false
+        ]
+        let login = testJson1["login"] as! String
+        let avatarUrl = testJson1["avatar_url"] as! String
+        XCTAssertEqual(login, "octocat")
+        XCTAssertEqual(avatarUrl, "https://github.com/images/error/octocat_happy.gif")
+        
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
