0590f4d2a76e7b9eeefa22a54132bddcbd201958
岩永 彩里 <>
2019-01-28 19:15:45 +0900

    Setting.swiftのテストコードを追加。

---
+++ GitHubAppTests/SettingTests.swift
@@ -0,0 +1,33 @@
+//
+//  SettingTests.swift
+//  GitHubAppTests
+//
+//  Created by 岩永 彩里 on 2019/01/28.
+//  Copyright © 2019年 岩永 彩里. All rights reserved.
+//
+
+import XCTest
+@testable import GitHubApp
+
+class SettingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testToken() {
+        let setting = Setting(defaults: UserDefaults.standard)
+        setting.token = "test"
+        XCTAssertEqual(setting.token, "test")
+    }
+    
+    func testTokened() {
+        let setting = Setting(defaults: UserDefaults(suiteName: "SettingTests")!)
+        XCTAssertEqual(setting.token, "")
+    }
+
+}
