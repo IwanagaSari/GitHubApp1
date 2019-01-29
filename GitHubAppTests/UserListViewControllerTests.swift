66813f6c575a429d1d8ae8491d6557fec65f14e1
岩永 彩里 <>
2019-01-29 20:45:30 +0900

    UserListViewのテーブルセルに関するテストを追加

--- GitHubAppTests/UserListViewControllerTests.swift
+++ GitHubAppTests/UserListViewControllerTests.swift
@@ -11,28 +11,55 @@ import XCTest
 
 protocol GitHubAPIType {
     func fetchUsers(completion: @escaping (([User]?, Error?) -> Void))
-}
+} //GitHubAPIのファイルに書くらしい。
 
 class DummyGitHubAPI: GitHubAPIType {
     
-    var userResult: ([User]?, Error?) //= empty
+    var userResult: ([User]?, Error?)
     
     func fetchUsers(completion: @escaping (([User]?, Error?) -> Void)) {
         completion(userResult.0, userResult.1)
-    }    
+    }
 }
 
 class UserListViewControllerTests: XCTestCase {
     
-    func testFetchUsers(completion: @escaping (([User]?, Error?) -> Void)) {
+    func testUserListView1(completion: @escaping (([User]?, Error?) -> Void)) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "UserListViewController") as? UserListViewController
+        XCTAssertNotNil(vc)
+        
         let dummyGitHubAPI = DummyGitHubAPI()
         let empty: ([User]?, Error?) = ([], nil)
         dummyGitHubAPI.userResult = empty
         
+        vc?.loadViewIfNeeded()
+        
+        let cell = vc?.tableView.visibleCells[0] as! UserListCell
+        
         dummyGitHubAPI.fetchUsers(completion: { users, error in
-            let usersCount = users?.count
-            XCTAssertNil(error)
-            XCTAssertEqual(usersCount, 0)            
+            let user = users?[0]
+            XCTAssertEqual(cell.userNameLabel.text, user?.userName)
+        })
+    }
+    
+    func testUserListView2(completion: @escaping (([User]?, Error?) -> Void)) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "UserListViewController") as? UserListViewController
+        XCTAssertNotNil(vc)
+        
+        let dummyGitHubAPI = DummyGitHubAPI()
+        
+        let user = User(userName: "name", image: "image")
+        let fourUsers: ([User]?, Error?) = ([user, user, user, user], nil)
+        dummyGitHubAPI.userResult = fourUsers
+        
+        vc?.loadViewIfNeeded()
+        
+        dummyGitHubAPI.fetchUsers(completion: { users, error in
+            let cell = vc?.tableView.visibleCells[3] as! UserListCell
+            let user3 = users?[3]
+            XCTAssertEqual(cell.userNameLabel.text, user3?.userName)
         })
     }
 }
