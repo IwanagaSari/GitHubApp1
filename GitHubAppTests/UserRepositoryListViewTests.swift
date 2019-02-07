94a6520a94a47a0c3aaacf340ef4ebae80e4102e
岩永 彩里 <>
2019-02-07 22:22:46 +0900

    UserRepositoryListViewTestsにテストコードを追加

--- GitHubAppTests/UserRepositoryListViewTests.swift
+++ GitHubAppTests/UserRepositoryListViewTests.swift
@@ -9,16 +9,6 @@
 import XCTest
 @testable import GitHubApp
 
-//class DummyGitHubAPI: GitHubAPITypeが入る {
-//
-//    var userResult: (UserDetail?, Error?)
-//
-//    func fetchUser(completion: @escaping ((UserDetail?, Error?) -> Void)) {
-//        completion(self.userResult.0, self.userResult.1)
-//    }
-//
-//}
-
 class UserRepositoryListViewTests: XCTestCase {
 
     override func setUp() {
@@ -30,15 +20,14 @@ class UserRepositoryListViewTests: XCTestCase {
         let vc = storyboard.instantiateViewController(withIdentifier: "UserRepositoryListViewController") as? UserRepositoryListViewController
         XCTAssertNotNil(vc)
         
-        //let api = DummyGitHubAPI()
-        //        vc?.gitHubAPI = api
-        //
-        //        api.userResult = ([], nil)
-        //
-        //        vc?.loadViewIfNeeded()
-        //
-        //        let number = vc?.tableView.numberOfRows(inSection: 0)
-        //        XCTAssertEqual(number, 0)
+        let api = DummyGitHubAPI()
+        vc?.gitHubAPI = api
+        api.userResult = (nil, nil) //nilでいいのか？
+        
+        vc?.loadViewIfNeeded()
+        
+        let number = vc?.repoTableView.numberOfRows(inSection: 0)
+        XCTAssertEqual(number, 0)
     }
     
     func testUserIsOne() {
@@ -46,14 +35,18 @@ class UserRepositoryListViewTests: XCTestCase {
         let vc = storyboard.instantiateViewController(withIdentifier: "UserRepositoryListViewController") as? UserRepositoryListViewController
         XCTAssertNotNil(vc)
         
-//        let api = DummyGithubAPI()
-//        vc?.loadViewIfNeeded()
-//
-//        let number = vc?.repoTableView.numberOfRows(inSection: 0)
-//        XCTAssertEqual(number, 1)
-//
-//        let indexPath = IndexPath(row: 0, section: 0)
-//        let cell = vc?.repoTableView.cellForRow(at: indexPath)
+        let api = DummyGitHubAPI()
+        vc?.gitHubAPI = api
+        let user = UserDetail(fullName: "fullName", followers: 1, following: 1, image: "image")
+        api.userResult = (user, nil)
+        vc?.loadViewIfNeeded()
+        
+        let number = vc?.repoTableView.numberOfRows(inSection: 0)
+        XCTAssertEqual(number, 1)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        let cell = vc?.repoTableView.cellForRow(at: indexPath)
+        //XCTAssertEqual(cell.repoLabel, <#T##expression2: Equatable##Equatable#>)
         
     }
 
